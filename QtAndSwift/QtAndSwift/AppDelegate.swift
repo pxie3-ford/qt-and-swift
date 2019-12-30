b83bf72ede4ea8d16dd451dd7e4e2424f580e042
Peng Xie <PXIE3@ford.com>
2019-12-30 14:21:36 -0500

    Clean up.

--- QtAndSwift/QtAndSwift/AppDelegate.swift
+++ QtAndSwift/QtAndSwift/AppDelegate.swift
@@ -12,15 +12,6 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
-//    var qtView: NSView?
-
-//    func applicationDidFinishLaunching(_ notification: Notification) {
-//        qtView = createQtApplicationView()
-//        window.contentView = qtView
-//    }
-//
-//    func applicationWillTerminate(_ notification: Notification) {
-//        destroyQtApplicationView(qtView!)
-//    }
+    
 }
 
